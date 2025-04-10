679de00a2c593ee7df015a5bebb9b2190f94f176
falk-adam <falk47.adam@gmail.com>
2025-04-10 22:24:17 +0200

    cv done

---
+++ script.js
@@ -0,0 +1,66 @@
+// Slidehow
+// referens till w3school: https://www.w3schools.com/howto/howto_js_slideshow.asp
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
+
+// dropdown jobb info
+// referens till w3school: https://www.w3schools.com/HOWTO/howto_js_toggle_hide_show.asp
+// måste finnas något smartare sätt än att upprepa det om och om men kunde ej lösa det
+function jobbInfo1() {
+    var x = document.getElementById("jobb1");
+    if (x.style.display === "none" || x.style.display === "") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+function jobbInfo2() {
+    var x = document.getElementById("jobb2");
+    if (x.style.display === "none" || x.style.display === "") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+function jobbInfo3() {
+    var x = document.getElementById("jobb3");
+    if (x.style.display === "none" || x.style.display === "") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+function jobbInfo4() {
+    var x = document.getElementById("jobb4");
+    if (x.style.display === "none" || x.style.display === "") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
